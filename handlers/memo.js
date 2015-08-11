ecec21d0899ed2b68200d6e26e0cc27c298aa195
hyva <sungcheon.lee@outlook.com>
2015-08-11 20:30:19 +0900

    1st mongo test

--- handlers/memo.js
+++ handlers/memo.js
@@ -1,16 +1,12 @@
 /** memoHandler.js **/   
-  
-var Datastore = require('nedb');
 
-//var server = new mongodb.Server('localhost', 27017, {});
-//var db = new mongodb.Db('mydatabase', server, {w: 1});
-
-var db = new Datastore({ filename : './data/memo', autoload : true});
 var querystring = require('querystring'); 
-var url = require('url');   
- 
- 
- 
+var url = require('url');
+var mongodb = require('mongodb');
+
+var server = new mongodb.Server('localhost', 27017, {});
+var db = new mongodb.Db('mydatabase', server, {w: 1});
+
 exports.create = function(req, res, body) { 
     _insertMemo(body, function(error, result) { 
         res.writeHead(200, { "Content-Type": "application/json" }); 
@@ -43,7 +39,6 @@ exports.remove = function(req, res, body) { 
     var where = querystring.parse(query);  
  
     console.log(where);
- 
     console.log(body);
 
     _removeMemo(where, function(error, results) { 
@@ -55,7 +50,7 @@ exports.remove = function(req, res, body) { 
  
 function _insertMemo(body, callback) { 
     body = typeof body === 'string' ? JSON.parse(body) : body;  
- 
+
     console.log(body.author);
     console.log(body.memo);
 
@@ -65,64 +60,54 @@ function _insertMemo(body, callback) { 
          date: new Date() 
     };  
     
-    //mongodb only
-    // db.open(function(err) {
-    //  if (err) throw err;
-    //     db.collection('memo').insert(memo , function(err, inserted){
-    //         if (err) throw err;
-
-    //         console.dir("successfully inserted: " + JSON.stringify(inserted));
-
-    //         db.close();
-
-    //         callback();
-
-
-    //     });
-    // });
-    db.insert(memo, callback);
+    db.open(function(err) {
+     if (err) throw err;
+        db.collection('memo').insert(memo , function(err, inserted){
+            if (err) throw err;
+            console.dir("successfully inserted: " + JSON.stringify(inserted));
+            db.close();
+            callback();
+        });
+    });
 }  
  
 function _findMemo(where, callback) { 
     where = where || {} 
-    //mongodb only
-    // db.open(function(err) {
-    //     if (err) throw err;
-    //     console.log("where " + where.toString());
-    //     db.collection('memo').find(where).toArray(function (err,docs){
-    //         console.dir(docs);
-    //         db.close();
-    //         callback(null, docs);
-    //     });
-    // });
-    db.find(where, callback);
+
+    db.open(function(err) {
+        if (err) throw err;
+        console.log("where " + where.toString());
+        db.collection('memo').find(where).toArray(function (err,docs){
+            console.dir(docs);
+            db.close();
+            callback(null, docs);
+        });
+    });
 }  
  
 function _updateMemo(where, body, callback) { 
     body = typeof body === 'string' ? JSON.parse(body) : body;
-    //mongodb only
-    // db.open(function(err) {
-    //     if (err) throw err;
-    //     db.collection('memo').update(where, { $set: body }, {multi: true}, function(err, updated) {
-    //         if (err) throw err;
-    //         console.dir("Successfully updated " + updated + " document!");
-    //         db.close();    
-    //         callback();
-    //     });
-    // });
-    db.update(where, {multi : true}, callback);
+
+    db.open(function(err) {
+        if (err) throw err;
+        //multi:true에서 오류를 이유로 false로 임시 처리함. 왜?
+        db.collection('memo').update(where, { $set: body }, {multi: false}, function(err, updated) {
+            if (err) throw err;
+            console.dir("Successfully updated " + updated + " document!");
+            db.close();    
+            callback();
+        });
+    });
 }  
  
 function _removeMemo(where, callback) { 
-    //mongodb only
-    // db.open(function(err) {
-    //     if (err) throw err; 
-    //     db.collection('memo').remove(where, { multi: true}, function(err, removed) {
-    //         if(err) throw err;
-    //         console.dir("Successfully deleted " + removed + " documents!");
-    //         db.close();
-    //         callback();
-    //     }); 
-    // });
-    db.remove(where, {multi:true}, callback);
+    db.open(function(err) {
+        if (err) throw err; 
+        db.collection('memo').remove(where, { multi: true}, function(err, removed) {
+            if(err) throw err;
+            console.dir("Successfully deleted " + removed + " documents!");
+            db.close();
+            callback();
+        }); 
+    });
 }
